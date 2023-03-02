cbe234d7b25bf0a83823667c435422c53cf17c1c
lubigsis <lucre.baluc@gmail.com>
2023-03-02 03:01:04 -0300

    agregué las otras teclas de direcciones

--- main.js
+++ main.js
@@ -3,12 +3,28 @@ window.onload = () => {
         elemento : document.getElementById('playerJS'),
         topPosition : 0,
         leftPosition : 0, /*----------------------p/controlar la posici+on del div*/
+        step : 25, //velocidad del div
         mover : function(direccion){  /*p/evaluar lo q' le llegue*/
             switch (direccion){
-                case 'ArrowFunction':
-                    console.log('Me muevo hacia arriba');
+                case 'ArrowUp':
+                    this.topPosition = this.topPosition - this.step;
+                    this.elemento.style.top = this.topPosition + 'px'; /*p/actualizar CSS*/
                     break;
 
+                case 'ArrowDown':
+                    this.topPosition = this.topPosition + this.step;
+                    this.elemento.style.top = this.topPosition + 'px'; 
+                    break;
+
+                case 'ArrowRight':
+                    this.leftPosition = this.leftPosition + this.step;
+                    this.elemento.style.left = this.leftPosition + 'px'; 
+                    break;
+
+                case 'ArrowLeft':
+                    this.leftPosition = this.leftPosition - this.step;
+                    this.elemento.style.left = this.leftPosition + 'px'; 
+                    break;
 
                 default:
                     break;
